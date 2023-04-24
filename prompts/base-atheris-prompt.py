94e8a33098fc6422913ab71b97df47ba5911134e
X <xacadena@gmail.com>
2023-04-23 19:41:22 -0500

    changes to prompt

--- prompts/base-atheris-prompt.py
+++ prompts/base-atheris-prompt.py
@@ -25,33 +25,35 @@ if __name__ == "__main__":
     atheris.instrument_all() # This is needed for coverage to work.
     main()
 
-# When fuzzing Python, Atheris will report a failure if the Python code under test throws an uncaught exception.
-# Atheris FuzzedDataProvider API Reference
-# The FuzzedDataProvider is a class that provides a number of functions to consume bytes from the input and convert them into other forms.
-# To construct the FuzzedDataProvider, use the following code:
+When fuzzing Python, Atheris will report a failure if the Python code under test throws an uncaught exception.
+Atheris FuzzedDataProvider API Reference:
+The FuzzedDataProvider is a class that provides a number of functions to consume bytes from the input and convert them into other forms.
+
+To construct the FuzzedDataProvider, use the following code:
 fdp = atheris.FuzzedDataProvider(data)
-# The FuzzedDataProvider provides the following functions, arguments are required unless otherwise specified:
-# default arguments for int should be sys.maxsize like ConsumeInt(sys.maxsize)
-# ConsumeBytes(count: int): Consume count bytes.
-# ConsumeUnicode(count: int): Consume unicode characters. Might contain surrogate pair characters, which according to the specification are invalid in this situation. However, many core software tools (e.g. Windows file paths) support them, so other software often needs to too.
-# ConsumeUnicodeNoSurrogates(count: int): Consume unicode characters, but never generate surrogate pair characters.
-# ConsumeString(count: int): Alias for ConsumeBytes in Python 2, or ConsumeUnicode in Python 3.
-# ConsumeInt(int: bytes): Consume a signed integer of the specified size (when written in two's complement notation).
-# ConsumeUInt(int: bytes): Consume an unsigned integer of the specified size.
-# ConsumeIntInRange(min: int, max: int): Consume an integer in the range [min, max].
-# ConsumeIntList(count: int, bytes: int): Consume a list of count integers of size bytes.
-# ConsumeIntListInRange(count: int, min: int, max: int): Consume a list of count integers in the range [min, max].
-# ConsumeFloat(): Consume an arbitrary floating#point value. Might produce weird values like NaN and Inf.
-# ConsumeRegularFloat(): Consume an arbitrary numeric floating#point value; never produces a special type like NaN or Inf.
-# ConsumeProbability(): Consume a floating#point value in the range [0, 1].
-# ConsumeFloatInRange(min: float, max: float): Consume a floating#point value in the range [min, max].
-# ConsumeFloatList(count: int): Consume a list of count arbitrary floating#point values. Might produce weird values like NaN and Inf.
-# ConsumeRegularFloatList(count: int): Consume a list of count arbitrary numeric floating#point values; never produces special types like NaN or Inf.
-# ConsumeProbabilityList(count: int): Consume a list of count floats in the range [0, 1].
-# ConsumeFloatListInRange(count: int, min: float, max: float): Consume a list of count floats in the range [min, max].
-# PickValueInList(l: list): Given a list, pick a random value.
-# ConsumeBool(): Consume either True or False.
-# An example of fuzzing with a custom mutator in Python:
+IMPORTANT: The FuzzedDataProvider arguments are required unless otherwise specified,default arguments for int should be sys.maxsize like ConsumeInt(sys.maxsize)
+The FuzzedDataProvider provides the following functions
+ConsumeBytes(count: int): Consume count bytes.
+ConsumeUnicode(count: int): Consume unicode characters. Might contain surrogate pair characters.
+ConsumeUnicodeNoSurrogates(count: int): Consume unicode characters, but never generate surrogate pair characters.
+ConsumeString(count: int): Alias for ConsumeBytes in Python 2, or ConsumeUnicode in Python 3.
+ConsumeInt(int: bytes): Consume a signed integer of the specified size (when written in two's complement notation).
+ConsumeUInt(int: bytes): Consume an unsigned integer of the specified size.
+ConsumeIntInRange(min: int, max: int): Consume an integer in the range [min, max].
+ConsumeIntList(count: int, bytes: int): Consume a list of count integers of size bytes.
+ConsumeIntListInRange(count: int, min: int, max: int): Consume a list of count integers in the range [min, max].
+ConsumeFloat(): Consume an arbitrary floating point value. Might produce weird values like NaN and Inf.
+ConsumeRegularFloat(): Consume an arbitrary numeric floating point value; never produces a special type like NaN or Inf.
+ConsumeProbability(): Consume a floating point value in the range [0, 1].
+ConsumeFloatInRange(min: float, max: float): Consume a floating point value in the range [min, max].
+ConsumeFloatList(count: int): Consume a list of count arbitrary floating point values. Might produce weird values like NaN and Inf.
+ConsumeRegularFloatList(count: int): Consume a list of count arbitrary numeric floating point values; never produces special types like NaN or Inf.
+ConsumeProbabilityList(count: int): Consume a list of count floats in the range [0, 1].
+ConsumeFloatListInRange(count: int, min: float, max: float): Consume a list of count floats in the range [min, max].
+PickValueInList(l: list): Given a list, pick a random value.
+ConsumeBool(): Consume either True or False.
+
+An example of fuzzing with a custom mutator in Python:
 import atheris
 import sys
 import zlib
@@ -88,7 +90,7 @@ if __name__ == "__main__":
     atheris.instrument_all()
     main()
 
-Make sure all fuzz test instrumented with the following code:
+IMPORTANT: ALWAYS use atheris.instrument_all() before calling main() to ensure that coverage is collected. This is required for coverage to work.
 
  def main():
     atheris.Setup(sys.argv, TestOneInput)
